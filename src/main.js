af547d2b978b0025dd9fa70c00eaa1aac9fadbd3
mijailCR12 <66917852+mijailCR12@users.noreply.github.com>
2023-09-28 22:08:44 -0600

    1

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import './assets/css/skeleton.css';
 
 const app = createApp(App);
   
-app.config.globalProperties.url = 'https://bookstore-mongo.netlify.app';
-
+app.config.globalProperties.url = 'https://spectacular-tanuki-865b1c.netlify.app';
+//https://bookstore-mongo.netlify.app
 app.use(router).mount("#app");
