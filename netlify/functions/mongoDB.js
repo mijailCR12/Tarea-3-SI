dd8f80c236f1776abf3afd3d5ce9dbbadc7002a8
mijailCR12 <66917852+mijailCR12@users.noreply.github.com>
2023-09-28 22:11:58 -0600

    2

--- netlify/functions/mongoDB.js
+++ netlify/functions/mongoDB.js
@@ -1,35 +1,35 @@
 "use strict";
 
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
-const client = new MongoClient(process.env.MONGODB_URI,
-  { useNewUrlParser: true,  useUnifiedTopology: true });
+// const client = new MongoClient(process.env.MONGODB_URI,
+//   { useNewUrlParser: true,  useUnifiedTopology: true });
   
-module.exports = client.connect();
+// module.exports = client.connect();
 
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://admin:<admin>@cluster0.6l3gxpe.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:<admin>@cluster0.6l3gxpe.mongodb.net/?retryWrites=true&w=majority";
 
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
