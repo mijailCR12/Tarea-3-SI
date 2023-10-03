8ce2dd5e5581bbc6a0c6b96d2282121ed65b1427
mijailCR12 <66917852+mijailCR12@users.noreply.github.com>
2023-10-02 19:40:15 -0600

    Revert "dele"
    This reverts commit c5526f96c9ffc9c52e39239ce39148bdf0beb6fc.

--- netlify/functions/bookDelete.js
+++ netlify/functions/bookDelete.js
@@ -1,63 +1,22 @@
-"use strict";
+"use strict"
 
-const { MongoClient } = require("mongodb");
-const headers = require("./headersCORS");
+const clientPromise = require('./mongoDB');
+const headers = require('./headersCORS');
 
 exports.handler = async (event, context) => {
-  if (event.httpMethod === "OPTIONS") {
+
+  if (event.httpMethod == "OPTIONS") {
     return { statusCode: 200, headers, body: "OK" };
   }
-
+	
   try {
-    // Establece la conexión a MongoDB
-    const client = new MongoClient(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    await client.connect();
-
+    const client = await clientPromise;
     const id = parseInt(event.path.split("/").reverse()[0]);
-
-    // Intenta eliminar el libro con el ID proporcionado
-    const result = await client.db("tarea").collection("books").findOneAndDelete({ _id: id });
-
-    if (result.value) {
-      console.log("Libro eliminado:", result.value);
-      return { statusCode: 200, headers, body: JSON.stringify(result.value) };
-    } else {
-      console.log("Libro no encontrado");
-      return { statusCode: 404, headers, body: "Libro no encontrado" };
-    }
+	
+    const authors = await client.db("tarea").collection("books").find({}).toArray();
+    return { statusCode: 200, headers, body: 'OK'};
   } catch (error) {
-    console.error("Error al eliminar el libro:", error);
-    return { statusCode: 500, headers, body: JSON.stringify(error) };
-  } finally {
-    // Cierra la conexión a MongoDB después de realizar la operación
-    if (client) {
-      client.close();
-    }
+    console.log(error);
+    return { statusCode: 422, headers, body: JSON.stringify(error) };
   }
 };
-
-// "use strict"
-
-// const clientPromise = require('./mongoDB');
-// const headers = require('./headersCORS');
-
-// exports.handler = async (event, context) => {
-
-//   if (event.httpMethod == "OPTIONS") {
-//     return { statusCode: 200, headers, body: "OK" };
-//   }
-	
-//   try {
-//     const client = await clientPromise;
-//     const id = parseInt(event.path.split("/").reverse()[0]);
-	
-//     const authors = await client.db("tarea").collection("books").findOneAndDelete({_id:id});
-//     return { statusCode: 200, headers, body: JSON.stringify(authors)};
-//   } catch (error) {
-//     console.log(error);
-//     return { statusCode: 422, headers, body: JSON.stringify(error) };
-//   }
-// };
---
+++ src/router.js
@@ -0,0 +1,56 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./components/Home.vue";
+import BookIndex from "./components/BookIndex.vue";
+import BookDetails from "./components/BookDetails.vue";
+
+import AuthorIndex from "./components/AuthorIndex.vue";
+import AuthorDetails from "./components/AuthorDetails.vue";
+
+import PublisherIndex from "./components/PublisherIndex.vue";
+import PublisherDetails from "./components/PublisherDetails.vue";
+
+const routes = [
+  { path: "/", component: Home },
+	
+  // books
+  { path: "/book", component: BookIndex },
+  { path: "/book/show/:id", 
+    component: BookDetails, props: {show:true} },
+  { path: "/book/edit/:id", 
+    component: BookDetails, props: {edit:true} },
+  { path: "/book/create", 
+    component: BookDetails, props: {create:true} },
+  { path: "/book/delete/:id", 
+    component: BookDetails, props: {delete:true} },
+
+  // authors
+  { path: "/author", component: AuthorIndex },
+  { path: "/author/show/:id", 
+    component: AuthorDetails, props: {show:true} },
+  { path: "/author/edit/:id", 
+    component: AuthorDetails, props: {edit:true} },
+  { path: "/author/create", 
+    component: AuthorDetails, props: {create:true} },
+  { path: "/author/delete/:id", 
+    component: AuthorDetails, props: {delete:true} },
+
+  // publisher
+  { path: "/publisher", component: PublisherIndex },
+  { path: "/publisher/show/:id", 
+    component: PublisherDetails, props: {show:true} },
+  { path: "/publisher/edit/:id", 
+    component: PublisherDetails, props: {edit:true} },
+  { path: "/publisher/create", 
+    component: PublisherDetails, props: {create:true} },
+  { path: "/publisher/delete/:id", 
+    component: PublisherDetails, props: {delete:true} },
+];
+
+const history = createWebHistory();
+
+const router = createRouter({
+  history,
+  routes,
+});
+
+export default router;
