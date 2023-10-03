b7a65bf8ab21890b435e10530717d07e261b9035
mijailCR12 <66917852+mijailCR12@users.noreply.github.com>
2023-10-02 19:44:28 -0600

    all delete

--- netlify/functions/bookDelete.js
+++ netlify/functions/bookDelete.js
@@ -38,26 +38,3 @@ exports.handler = async (event, context) => {
     }
   }
 };
-
-// "use strict"
-
-// const clientPromise = require('./mongoDB');
-// const headers = require('./headersCORS');
-
-// exports.handler = async (event, context) => {
-
-//   if (event.httpMethod == "OPTIONS") {
-//     return { statusCode: 200, headers, body: "OK" };
-//   }
-	
-//   try {
-//     const client = await clientPromise;
-//     const id = parseInt(event.path.split("/").reverse()[0]);
-	
-//     const authors = await client.db("tarea").collection("books").find({}).toArray();
-//     return { statusCode: 200, headers, body: 'OK'};
-//   } catch (error) {
-//     console.log(error);
-//     return { statusCode: 422, headers, body: JSON.stringify(error) };
-//   }
-// };
--- netlify/functions/publisherDelete.js
+++ netlify/functions/publisherDelete.js
@@ -1,22 +1,40 @@
-"use strict"
+"use strict";
 
-const clientPromise = require('./mongoDB');
-const headers = require('./headersCORS');
+const { MongoClient } = require("mongodb");
+const headers = require("./headersCORS");
 
 exports.handler = async (event, context) => {
-
-  if (event.httpMethod == "OPTIONS") {
+  if (event.httpMethod === "OPTIONS") {
     return { statusCode: 200, headers, body: "OK" };
   }
-	
+
   try {
-    const client = await clientPromise;
+    // Establece la conexión a MongoDB
+    const client = new MongoClient(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+
     const id = parseInt(event.path.split("/").reverse()[0]);
-	
-    const authors = await client.db("tarea").collection("publishers").find({}).toArray();
-    return { statusCode: 200, headers, body: 'OK'};
+
+    // Intenta eliminar el editor con el ID proporcionado
+    const result = await client.db("tarea").collection("publishers").findOneAndDelete({ _id: id });
+
+    if (result.value) {
+      console.log("Editor eliminado:", result.value);
+      return { statusCode: 200, headers, body: JSON.stringify(result.value) };
+    } else {
+      console.log("Editor no encontrado");
+      return { statusCode: 404, headers, body: "Editor no encontrado" };
+    }
   } catch (error) {
-    console.log(error);
-    return { statusCode: 422, headers, body: JSON.stringify(error) };
+    console.error("Error al eliminar el editor:", error);
+    return { statusCode: 500, headers, body: JSON.stringify(error) };
+  } finally {
+    // Cierra la conexión a MongoDB después de realizar la operación
+    if (client) {
+      client.close();
+    }
   }
 };
